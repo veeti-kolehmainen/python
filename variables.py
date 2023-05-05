3907d750fc236ec80506d9cf96353051fea11adb
veeti-kolehmainen <veetikolehmainen@gmail.com>
2023-05-05 08:48:09 +0300

    Tried out basic stuff

---
+++ variables.py
@@ -0,0 +1,10 @@
+#age = 20
+#age = 19.95
+#first_name = "Veeti"
+#is_online = True
+#print(age)
+
+fullName = "John Smith"
+age = 20
+newPatient = True
+print(fullName, "is", age, "years old. Is he a new patient?:", newPatient)
