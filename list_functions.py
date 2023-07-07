18a4141d5a87bfef2a59235a0511c06f5f1a0a63
veeti-kolehmainen <veetikolehmainen@gmail.com>
2023-07-07 13:23:34 +0300

    tested some list functions

---
+++ list_functions.py
@@ -0,0 +1,17 @@
+lucky_numbers = [8, 4, 15, 16, 23, 42]
+friends = ["Kevin", "Karen", "Jim", "Jim", "Oscar", "Tim"]
+
+friends2 = friends.copy()
+
+# lucky_numbers.reverse()
+# lucky_numbers.sort()
+# print(friends.count("Jim"))
+# print(friends.index("Kevin"))
+# friends.pop()
+# friends.clear()
+# friends.remove("Jim")
+# friends.insert(1, "Kelly")
+# friends.append("Creed")
+# friends.extend(lucky_numbers)
+
+print(friends2)
