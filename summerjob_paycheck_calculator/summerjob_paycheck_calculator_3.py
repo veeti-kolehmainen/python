d539e08cb546f56947a1e12fe8874563e79c1ae8
veeti-kolehmainen <veetikolehmainen@gmail.com>
2023-07-09 17:08:18 +0300

    made a summerjob paycheck calculator (again) but with a while loop this time

---
+++ summerjob_paycheck_calculator/summerjob_paycheck_calculator_3.py
@@ -0,0 +1,25 @@
+# This is a paycheck calculator for my newspaper and advertisement delivery summerjob
+# I did not calcualate taxes because I do not make over 10 000€ yearly
+
+
+# Paycheck details in my contract
+first_product = 16.89
+next_products = 2.20
+km_coverage = 7.80
+extras = first_product + km_coverage
+
+# Variables for the while loop
+i = 1
+total_pay = 0
+week_number = 1
+
+# While loop that calculates my paycheck
+while i <= 4:
+    pay = int(input("Week " + str(week_number) + " product amount: "))
+    pay = (pay - 1) * next_products + extras
+    total_pay = total_pay + pay
+    i += 1
+    week_number += 1
+
+# Prints the total paycheck
+print("Your paycheck should be: " + str(total_pay) + "€")
