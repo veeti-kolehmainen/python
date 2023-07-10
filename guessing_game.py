5b4040d407b7e64bb3ef476729dbdc35642f7048
veeti-kolehmainen <veetikolehmainen@gmail.com>
2023-07-10 18:46:10 +0300

    updated the game to have lives

--- guessing_game.py
+++ guessing_game.py
@@ -1,9 +1,19 @@
 secret_word = "giraffe"
 guess = ""
+guess_lives = 3
+guess_limit = 0
+out_of_guesses = False
 
-while guess != secret_word:
-    guess = input("Enter guess: ")
-    if guess != secret_word:
-        print("Try again.")
+while guess != secret_word and not(out_of_guesses):
+    if guess_lives > guess_limit:
+        guess = input("Enter guess: ")
+        guess_lives -= 1
+    else:
+        out_of_guesses = True
+    if guess != secret_word and not(out_of_guesses):
+        print("(" + str(guess_lives) + " lives left)")
 
-print("You win!")
+if out_of_guesses:
+    print("Out of guesses, you lose!")
+else:
+    print("You win!")
