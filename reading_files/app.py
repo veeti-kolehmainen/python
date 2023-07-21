05dc49f2f74d594e6e75b2e8ef7a36d50910372a
veeti-kolehmainen <veetikolehmainen@gmail.com>
2023-07-22 01:31:31 +0300

    tested .readlines() and made it a for loop

--- reading_files/app.py
+++ reading_files/app.py
@@ -1,5 +1,7 @@
 employee_file = open("C:/Users/veeti/OneDrive - Luksia/html 2/python/reading_files/employee.txt", "r")
 
-print(employee_file.readable())
+for employee in employee_file.readlines():
+    print(employee)
+# print(employee_file.readlines()[2])
 
 employee_file.close()
