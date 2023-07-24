168e28c2ee6b2ea7005df752c955aa2985aabb5b
veeti-kolehmainen <veetikolehmainen@gmail.com>
2023-07-24 10:33:48 +0300

    renamed

--- reading_files/app.py
+++
@@ -1,7 +0,0 @@
-employee_file = open("C:/Users/veeti/OneDrive - Luksia/html 2/python/reading_files/employee.txt", "r")
-
-for employee in employee_file.readlines():
-    print(employee)
-# print(employee_file.readlines()[2])
-
-employee_file.close()
