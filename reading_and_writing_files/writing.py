b51875bef4d1ac9c7c17f53fbc05aa6f439d2cf8
veeti-kolehmainen <veetikolehmainen@gmail.com>
2023-07-24 10:33:24 +0300

    tested writing in files

---
+++ reading_and_writing_files/writing.py
@@ -0,0 +1,10 @@
+# employee_file = open("C:/Users/veeti/OneDrive - Luksia/html 2/python/reading_and_writing_files/employee_file1.txt", "w")
+
+# employee_file.write("\nKelly - Customer Service")
+
+# employee_file.close()
+
+
+employee_file = open("reading_and_writing_files/index.html", "w")
+
+employee_file.write("<html><head><title>Python page</title></head><body><h1>Hello, this page was made in python!</h1><p>by using .write</p></body>")
